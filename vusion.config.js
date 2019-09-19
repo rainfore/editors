72733517981db5bf1b36fef08ed5cf853e36e139
Rainfore <rainforest92@126.com>
2019-09-19 21:59:52 +0800

    :arrow_up: Upgrade package

---
+++ vusion.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    version: '>=0.8.2',
+    type: 'library',
+    name: 'editors',
+    CamelName: 'Editors',
+    docs: {
+        title: '编辑器',
+        logo: '编辑器',
+        mode: 'history',
+        base: '/editors/',
+        github: 'https://github.com/vusion/editors',
+        // navbar: [
+        //     { text: '组件', to: '/components' },
+        // ],
+    },
+};
